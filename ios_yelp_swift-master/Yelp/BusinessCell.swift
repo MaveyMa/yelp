696030336458dd988b8670f568e9b0ba93fce23f
Mavey Ma <maveyma@gmail.com>
2018-02-17 23:56:20 -0800

    Cells display basic labels of restaurant info

--- ios_yelp_swift-master/Yelp/BusinessCell.swift
+++ ios_yelp_swift-master/Yelp/BusinessCell.swift
@@ -9,18 +9,29 @@
 import UIKit
 
 class BusinessCell: UITableViewCell {
-
-  @IBOutlet weak var thumbImageView: UIImageView!
   
+  @IBOutlet weak var thumbImageView: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var distanceLabel: UILabel!
+  @IBOutlet weak var reviewsCountLabel: UILabel!
+  @IBOutlet weak var addressLabel: UILabel!
+  @IBOutlet weak var categoriesLabel: UILabel!
+  @IBOutlet weak var ratingImageView: UIImageView!
+  
+  var business: Business! {
+    didSet {
+      nameLabel.text = business.name
+    }
+  }
   override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+  
 }
